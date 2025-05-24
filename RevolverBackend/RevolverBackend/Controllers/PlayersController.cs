dc14488857925ac1d761f1ffe86e5c2486e5f98c
PiltanStudio <sajjadbaradaraneftekhari@gmail.com>
2025-05-25 01:16:56 +0330

    Player Service Injection Complete

--- RevolverBackend/RevolverBackend/Controllers/PlayersController.cs
+++ RevolverBackend/RevolverBackend/Controllers/PlayersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RevolverBackend.Models;
+using RevolverBackend.Services;
 
 namespace RevolverBackend.Controllers
 {
@@ -9,16 +10,17 @@ namespace RevolverBackend.Controllers
     public class PlayersController : ControllerBase
     {
         private static List<Player> players = new List<Player>();
+        private readonly IPlayerService _playerService;
+
+        public PlayersController(IPlayerService playerService)
+        {
+            _playerService = playerService;
+        }
 
         [HttpGet]
-        public IActionResult Get() => Ok(players);
+        public IActionResult GetAll() => Ok(_playerService.GetAll());
 
         [HttpPost]
-        public IActionResult Add(Player player)
-        {
-            player.Id = players.Count + 1;
-            players.Add(player);
-            return Ok(player);
-        }
+        public IActionResult Add(Player player) => Ok(_playerService.Add(player));
     }
 }
