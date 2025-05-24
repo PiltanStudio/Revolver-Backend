f97b1a738c7872d70f4c09bd67f52508570186b2
PiltanStudio <sajjadbaradaraneftekhari@gmail.com>
2025-05-24 21:05:32 +0330

    created a player model and a controller

---
+++ RevolverBackend/RevolverBackend/Controllers/PlayersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RevolverBackend.Models;
+
+namespace RevolverBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        private static List<Player> players = new List<Player>();
+
+        [HttpGet]
+        public IActionResult Get() => Ok(players);
+
+        [HttpPost]
+        public IActionResult Add(Player player)
+        {
+            player.Id = players.Count + 1;
+            players.Add(player);
+            return Ok(player);
+        }
+    }
+}
