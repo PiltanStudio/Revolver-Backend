f7c891dae201e18dcca0a2f40dc716442ee81665
PiltanStudio <sajjadbaradaraneftekhari@gmail.com>
2025-05-27 15:27:07 +0330

    Added Nuget files and a Data file

---
+++ RevolverBackend/RevolverBackend/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using RevolverBackend.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace FingerDuelBackend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }  // Create a table for Player
+    }
+}
