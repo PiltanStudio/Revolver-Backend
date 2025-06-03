01a77031f9401bf8e97e1f80c06953634bb002db
PiltanStudio <sajjadbaradaraneftekhari@gmail.com>
2025-06-03 17:54:58 +0330

    prepared auth

---
+++ RevolverBackend/RevolverBackend/Models/User.cs
@@ -0,0 +1,6 @@
+﻿namespace RevolverBackend.Models
+{
+    public class User
+    {
+    }
+}
--- RevolverBackend/RevolverBackend/Program.cs
+++ RevolverBackend/RevolverBackend/Program.cs
@@ -1,13 +1,38 @@
+<<<<<<< Updated upstream
 using RevolverBackend.Data;
 using Microsoft.EntityFrameworkCore;
+=======
+using Microsoft.IdentityModel.Tokens;
+>>>>>>> Stashed changes
 using RevolverBackend.Services;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+<<<<<<< Updated upstream
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+=======
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = "revolver-api",
+            ValidAudience = "revolver-client",
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("super-secret-key-12345"))
+        };
+    });
+
+>>>>>>> Stashed changes
 builder.Services.AddEndpointsApiExplorer();   // Required for Swagger
 builder.Services.AddSwaggerGen();            // Registers Swagger services
 builder.Services.AddScoped<IPlayerService, PlayerService>();
@@ -23,6 +48,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthorization();
 app.UseAuthorization();
 
 app.MapControllers();
